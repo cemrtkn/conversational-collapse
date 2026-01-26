76ae8d34719c8bbabefdac8c7fbb30f9230b9694
cemrtkn <cemrtkn@gmail.com>
2026-01-26 11:47:16 +0100

    Simplify SLURM scripts by mounting the repo into the container

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #
+#SBATCH --chdir=.
 #SBATCH --output logs/slurm-%x-%j.out
 #SBATCH --error logs/slurm-%x-%j.out
-#SBATCH -D ./
 #SBATCH --job-name collapse_experiment
 #
 #SBATCH --nodes=1
@@ -16,28 +16,15 @@
 # Wall clock limit (max is 24 hours):
 #SBATCH --time=00:30:00
 
-DEBUG_FLAG=$1
 # defaults to configs/test_config.yaml
-CONFIG_FILE=${2:-configs/test_config.yaml}
+CONFIG_FILE=${1:-configs/test_config.yaml}
 
-# Build debug argument
-if [ "$DEBUG_FLAG" = "debug" ]; then
-    DEBUG_ARG="--debug"
-else
-    DEBUG_ARG=""
-fi
 
-module load apptainer
+source .venv/bin/activate
 
 source .env
 
-INFERENCE_IMAGE="container_images/inference.sif"
+python -c "import torch; print('CUDA available:', torch.cuda.is_available()); print(torch.cuda.get_device_name(0))"
+python -c "from pyarrow import __version__; print('PyArrow version:', __version__)"
 
-apptainer exec --nv \
-    -B .:"$HOME" \
-    -B ./data:/data \
-    -B "$HF_CACHE_PATH:/huggingface" \
-    --env HF_TOKEN="${HF_TOKEN}" \
-    --env HF_HUB_CACHE="/huggingface/hub" \
-    "${INFERENCE_IMAGE}" \
-    bash -c 'pip install --user -e "$HOME" --no-deps && python src/main.py '"$CONFIG_FILE"' '"$DEBUG_ARG"
+python src/main.py "$CONFIG_FILE"
